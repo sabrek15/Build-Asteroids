a07dafbf1d3d395562bdf9d55c8fee41669d71aa
sabrek15 <reddy.ksb007@gmail.com>
2025-02-21 17:00:31 +0530

    added some pygame methods

--- main.py
+++ main.py
@@ -2,6 +2,19 @@ import pygame
 from constants import *
 
 def main():
+    pygame.get_init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    running = True
+    while running:
+        screen.fill((0, 0, 0))
+
+        pygame.display.flip()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
