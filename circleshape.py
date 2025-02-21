0791ab056940b0ed205f619413d58368644088a9
sabrek15 <reddy.ksb007@gmail.com>
2025-02-21 17:37:00 +0530

    added the circleshape.py that has circleshape class

---
+++ circleshape.py
@@ -0,0 +1,15 @@
+import pygame
+
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+    
+    def draw(self, screen):
+        pass
+
+    def update(self, dt):
+        pass
+
